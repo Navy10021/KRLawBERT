955c317c602fa35d15d912210d190c128da9e55b
Yoonseop Lee <navy10021@snu.ac.kr>
2023-10-03 20:58:06 +0900

    Update and rename advanced_dynamic-MLM.py to frequency-MLM.py

---
+++ pre-training/frequency-MLM.py
@@ -0,0 +1,3 @@
+
+# implements an advanced masking strategy based on token frequencies. We count the frequency of each token in the input text and sort tokens by frequency.
+# Then, we mask a portion (e.g., bottom 10%) of the less frequent tokens.
